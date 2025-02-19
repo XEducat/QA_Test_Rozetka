eab242deea2fbfae1f42e196b24756454f3a96b6
Vadim <113421299+XEducat@users.noreply.github.com>
2025-02-19 22:08:20 +0200

    Refactored CatalogPage

--- src/main/java/Tests/Pages/CatalogPage.java
+++ src/main/java/Tests/Pages/CatalogPage.java
@@ -11,6 +11,7 @@ import static org.testng.Assert.*;
 public class CatalogPage extends PageTools {
     public final String catalogURL = "https://rozetka.com.ua/ua/mobile-phones/c80003/producer=apple;series=iphone-15,iphone-15-plus,iphone-15-pro,iphone-15-pro-max/#search_text=iphone+15";
     private CatalogAction catalogAction = new CatalogAction();
+
     private ItemData foundedItem;
     private ItemPage itemPage = new ItemPage();
 
@@ -31,18 +32,5 @@ public class CatalogPage extends PageTools {
         assertNotNull(foundedItem.item, "Заголовок товару не знайдено!");
         assertEquals(foundedItem.titleText, itemPage.nameText, "Назва товару не співпадає!");
         assertEquals(foundedItem.priceText, itemPage.priceText, "Ціна товару не співпадає!");
-    }
-
-    // Метод для пошуку товару в каталозі
-    private SelenideElement findItemInCatalog(String itemName) {
-        try {
-            return $$x("//app-goods-tile-default")
-                    .findBy(Condition.text(itemName))
-                    .$x(".//rz-button-product-page[2]/rz-indexed-link")
-                    .shouldBe(Condition.exist, Condition.visible);
-        } catch (AssertionError e) {
-            fail("В каталозі не знайдено товару!");
-            return null; // Ніколи не виконається через fail()
-        }
     }
 }
